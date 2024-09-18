2f22a65503e222d684fcb881c93c110d98c09cc2
Ahmee139 <149863768+Ahmee139@users.noreply.github.com>
2024-09-18 14:14:33 +0500

    Add files via upload

---
+++ currentdate.js
@@ -0,0 +1,21 @@
+function currentDate() {
+    const current = new Date();
+    let monthName = current.toLocaleDateString('en-US', {
+        month: "long",
+    });
+
+    let date = current.getDate();
+
+    let dayName = current.toLocaleDateString('en-US', {
+        weekday: "long",
+    });
+
+    var monthText = document.getElementById("month");
+    monthText.innerHTML = monthName;
+
+    var dayText = document.getElementById("date");
+    dayText.innerHTML = date;
+
+    var weekDayText = document.getElementById("day");
+    weekDayText.innerText = dayName;
+}
